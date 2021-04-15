7144eb2d9e5dd8fc778dfdf8fa0ce1019b723a83
CorbenG <61744834+CorbenG@users.noreply.github.com>
2021-04-14 22:09:41 -0400

    Add ammo indicator under crosshair and nerf blaster
    Changed blaster projectile damage from 15 to 12

---
+++ Assets/FPS/Scripts/CrosshairAmmoTracker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrosshairAmmoTracker : MonoBehaviour
+{
+    [Tooltip("UI element displaying ammo count")]
+    public GameObject ammoCounter;
+
+    PlayerWeaponsManager m_PlayerWeaponsManager;
+    List<AmmoCounter> m_AmmoCounters = new List<AmmoCounter>();
+
+    WeaponController activeWeapon;
+
+    void Start()
+    {
+        m_PlayerWeaponsManager = FindObjectOfType<PlayerWeaponsManager>();
+        DebugUtility.HandleErrorIfNullFindObject<PlayerWeaponsManager, WeaponHUDManager>(m_PlayerWeaponsManager, this);
+    }
+
+    void Update()
+    {
+        activeWeapon = m_PlayerWeaponsManager.GetActiveWeapon();
+
+        ammoCounter.GetComponent<Image>().fillAmount = activeWeapon.currentAmmoRatio;
+    }
+
+
+}
